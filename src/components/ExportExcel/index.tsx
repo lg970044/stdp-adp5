6df9a71605dba4fa0ef8bcd91ebab21092c2cca2
liuxiong <lg970044@foxmail.com>
2021-12-27 16:02:51 +0800

    feat: 优化Excel导出，支持dataIndex数组

--- src/components/ExportExcel/index.tsx
+++ src/components/ExportExcel/index.tsx
@@ -1,18 +1,20 @@
 import XLSX from 'xlsx';
+import get from 'rc-util/es/utils/get';
 
 const parseValueEnum = (value: any, valueEnum: any) => {
   return valueEnum[value] ? valueEnum[value]?.text || valueEnum[value] : value;
 };
 
+const parseColValue = (row: any, header: any) => {
+  const value = Array.isArray(header.dataIndex)
+    ? get(row, header.dataIndex)
+    : row[header.dataIndex];
+  return header?.valueEnum ? parseValueEnum(value, header.valueEnum) : value;
+};
+
 const ExportExcel = (columns: any[], rows: any[], fileName = 'export.xlsx') => {
   const headers = columns.filter((col) => col?.hideInTable != true && col?.valueType != 'option');
-  const datas = rows.map((row) =>
-    headers.map((header) =>
-      header?.valueEnum
-        ? parseValueEnum(row[header.dataIndex], header.valueEnum)
-        : row[header.dataIndex],
-    ),
-  );
+  const datas = rows.map((row) => headers.map((header) => parseColValue(row, header)));
 
   const wb = XLSX.utils.book_new();
   const ws = XLSX.utils.aoa_to_sheet([headers.map((header) => header?.title), ...datas]);
